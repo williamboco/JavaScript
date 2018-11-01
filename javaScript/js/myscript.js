67573f787d07064c6d841030d25b7f997bf72601
williamboco <44660322+williamboco@users.noreply.github.com>
2018-11-01 11:36:26 +0800

    Add files via upload

---
+++ javaScript/js/myscript.js
@@ -0,0 +1,33 @@
+//Output
+document.write("Hello World!");
+document.write("<h1>Ako Budoy!</h1>");
+
+
+//This is a single line comment
+
+/*
+This code is for
+multiple
+comments*/
+//alert("This is an alert box!");
+
+var script = 21;
+document.write("<h1>" + script + "</h1>");
+
+//Integer
+var int = 18;
+document.write(int + "<br><br>");
+
+//Floating-Point
+var price = 55.55;
+document.write(price + "<br><br>");
+
+//String
+var name = "I am Yam";
+document.write("<h4>" + name + "</h4>" + "<br>");
+
+//Handling the quotes using the escape character (\);
+var sayHi = 'Hi World! \'I am a JavaScript Programmer.\'';
+document.write(sayHi);
+
+//eval("10 * 20 + 8");
